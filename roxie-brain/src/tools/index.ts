ca3c345be5976ddf3d02848ecaec8b59541c03aa
Rahul R <github.upload780@passinbox.com>
2026-02-15 17:44:46 +0530

    Backend MongoDB and QDrant setups deleted

--- roxie-brain/src/tools/index.ts
+++ roxie-brain/src/tools/index.ts
@@ -1,9 +1,3 @@
-import * as mongoDBTools from "../db/mongodb/tools";
-import * as qdrantTools from "../db/qdrant/tools";
-
 import getCurrentDateTime from "./getCurrentDateTime";
 
-const { userTools, groceryTools } = mongoDBTools;
-const {} = qdrantTools;
-
-export const tools = [getCurrentDateTime, ...userTools, ...groceryTools];
+export const tools = [getCurrentDateTime];
